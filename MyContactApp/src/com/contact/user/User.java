d7806321895867abca0b194aa57ee7b4ff059d6b
mayurpatil0906 <mayurpatill0906@gmail.com>
2026-02-27 10:46:47 +0530

    commit changes to show specific contact based on id

--- MyContactApp/src/com/contact/user/User.java
+++ MyContactApp/src/com/contact/user/User.java
@@ -33,31 +33,31 @@ public class User {
             throw new RuntimeException("Error hashing password");
         }
     }
-
+//to check password 
     public boolean checkPassword(String password) {
         return passwordHash.equals(hashPassword(password));
     }
-
+//getter method to get email
     public String getEmail() {
         return email;
     }
-
+  //getter method to get name
     public String getName() {
         return name;
     }
-
+  //getter method to set name
     public void setName(String name) {
         this.name = name;
     }
-
+  //getter method to set strong password
     public void setPassword(String newPassword) {
         this.passwordHash = hashPassword(newPassword);
     }
-
+//method to add contact
     public void addContact(Contact contact) {
         contacts.add(contact);
     }
-
+//list to get all contacts
     public List<Contact> getContacts() {
         return contacts;
     }
@@ -65,6 +65,7 @@ public class User {
     public String getUserType() {
         return "Normal User";
     }
+    // to view all contacts
     public void viewContacts() {
 
         if (contacts.isEmpty()) {
@@ -75,11 +76,25 @@ public class User {
         System.out.println("\n---- Your Contacts ----");
 
         for (Contact c : contacts) {
-            System.out.println("Name  : " + c.getName());
-            System.out.println("Phone : " + c.getPhone());
-            System.out.println("Email : " + c.getEmail());
-            System.out.println("-------------------------");
+            System.out.println("ID   : " + c.getId());
+            System.out.println("Name : " + c.getName());
+            System.out.println("Phone: " + c.getPhone());
+            System.out.println("Email: " + c.getEmail());
+            System.out.println("-----------------------");
         }
+    }
+    //after adding we will get the id using that id contact will be shown
+    public void viewContactById(String id) {
+
+        for (Contact c : contacts) {
+            if (c.getId().equals(id)) {
+                System.out.println("\n--- Contact Details ---");
+                c.display();
+                return;
+            }
+        }
+
+        System.out.println("Contact not found!");
     }
     
 }
